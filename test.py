00d6a0b6aa04bdacb93d661c16cae49ccb3e5431
swapna0211950 <75247490+swapna0211950@users.noreply.github.com>
2021-06-24 15:46:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,14 +3,12 @@
 def get_nested(data, key):
     if key and data:
         element  = key[0]
-        #print(element)
         if element:
-            value = data.get(element)
-            #print(value)
+            value = data.get(element)#fetch the value of each elemnt of the key
             return value if len(key) == 1 else get_nested(value, key[1:])
 
 obj={"a" : {"b" : {"c" : "d"}}}
 key_path= "a/b/c"
-paths = key_path.split("/")
+paths = key_path.split("/")#converting the path to list
 x=get_nested(obj, paths)
 print(x)
